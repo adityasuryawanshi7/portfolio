38b75d3a6439a31963ca28d76e7f03dfce56033c
ak15 <157221199+adityasuryawanshi7@users.noreply.github.com>
2025-02-13 02:26:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menuIcon');
+    const navLinks = document.getElementById('navLinks');
+  
+    menuIcon.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  });
+  
